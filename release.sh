d98f76573f36123d3f46d8931189040beaa48c00
cblgh <cblgh@cblgh.org>
2022-03-07 10:49:25 +0100

    improve release script, add fts5 tag flag

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ do
     if [ $os = "windows" ]; then
         executable="lieu.exe"
     fi
-    env GOOS="$os" go build -ldflags "-s -w"
+    env GOOS="$os" go build -tags fts5 -ldflags "-s -w"
     tar czf "lieu-$os.tar.gz" README.md html/ data/ lieu.toml "$executable"
     echo "lieu-$os.tar.gz"
     rm -f "$executable"
